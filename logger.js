46c06a42fb5cd61cc2a1b8d079eeff867da88e29
Upkar Lidder <ulidder@us.ibm.com>
2021-08-03 15:45:19 -0700

    formatting files and adding linter github action
    Signed-off-by: Upkar Lidder <ulidder@us.ibm.com>

--- logger.js
+++ logger.js
@@ -1,18 +1,19 @@
-'use strict';
+"use strict";
 
-const { createLogger, format, transports } = require('winston');
-const path = require('path');
+const { createLogger, format, transports } = require("winston");
+const path = require("path");
 
 const logger = createLogger({
-  level: 'debug',
+  level: "debug",
   format: format.combine(
     format.label({ label: path.basename(process.mainModule.filename) }),
-    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     format.printf(
-      info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
+      (info) =>
+        `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
     )
   ),
-  transports: [new transports.Console()]
+  transports: [new transports.Console()],
 });
 
 module.exports = logger;
